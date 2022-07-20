bf8f218edbcde1c3257d956c491e611526625fb8
Donal Hanna <donal@the-plot.com>
2022-07-20 19:59:05 +0100

    Delete setup.sql

--- wpScratch/setup.sql
+++
@@ -1,7 +0,0 @@
-DROP USER 'root'@'localhost';
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'P1ngal1ng!';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-flush privileges
-####
-create database wordpress;
-grant all privileges on *.* to 'wordy'@'localhost' identified by 'P1nkleP0nkle';
